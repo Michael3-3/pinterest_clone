70d3be59a67cf5855cf72b0775cacd111485587a
Michael3-3 <150669515+Michael3-3@users.noreply.github.com>
2024-06-26 12:37:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,163 @@
+var arr = [
+    {
+        name: "mountains",
+        image:
+            "https://images.unsplash.com/photo-1716321952293-caa23fcd4d96?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8",
+    },
+    {
+        name: "night city",
+        image:
+            "https://images.unsplash.com/photo-1716566130844-e93950e5aa55?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8",
+    },
+    {
+        name: "snow lands",
+        image:
+            "https://images.unsplash.com/photo-1716847214623-c45b2db2d569?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        name: "night sky",
+        image:
+            "https://images.unsplash.com/photo-1716847214624-1e8787d98b6c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8",
+    },
+    {
+        name: "space",
+        image:
+            "https://images.unsplash.com/photo-1716881139357-ddcb2f52940c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        name: "sea lands",
+        image:
+            "https://plus.unsplash.com/premium_photo-1675860538045-fe53a6cb75a8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxN3x8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        name: "city center"    ,
+        image:"https://images.unsplash.com/photo-1712688930249-98e1963af7bd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8"  
+    },
+    {
+        name:  "girl model"   ,
+        image:  "https://images.unsplash.com/photo-1611601322175-ef8ec8c85f01?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bW9kZWxzfGVufDB8fDB8fHww"
+    },
+    {
+        name:   "girl model"  ,
+        image:  "https://images.unsplash.com/photo-1622396636133-ba43f812bc35?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fG1vZGVsc3xlbnwwfHwwfHx8MA%3D%3D"
+    },
+    {
+        name:    "girl model" ,
+        image:  "https://images.unsplash.com/photo-1611558709798-e009c8fd7706?q=80&w=1891&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        name: "city model"    ,
+        image:  "https://images.unsplash.com/photo-1613991917221-4723976fa917?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDh8fG1vZGVsc3xlbnwwfHwwfHx8MA%3D%3D"
+    },
+    {
+        name:  "paint wallpaper"   ,
+        image:  "https://images.unsplash.com/photo-1561212044-bac5ef688a07?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDExfGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D"
+    },
+    {
+        name:  "let's create potery"   ,
+        image:  "https://images.unsplash.com/photo-1631125915902-d8abe9225ff2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEwfGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D"
+    },
+    {
+        name:  "china wall"   ,
+        image:  "https://images.unsplash.com/photo-1717649389735-da3f03987b7e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI1fGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D"
+    },
+    {
+        name:  "yello tree"   ,
+        image:  "https://images.unsplash.com/photo-1716908952387-0b1128538b68?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDM2fGJvOGpRS1RhRTBZfHxlbnwwfHx8fHw%3D"
+    },
+    {
+        name:   "model girl"  ,
+        image:  "https://images.unsplash.com/photo-1611601679655-7c8bc197f0c6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9kZWwlMjBnaXJsfGVufDB8fDB8fHww"
+    },
+    {
+        name:    "model girl" ,
+        image:  "https://images.unsplash.com/photo-1630255733678-cf82ac59eef8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG1vZGVsJTIwZ2lybHxlbnwwfHwwfHx8MA%3D%3D"
+    },
+    {
+        name:    "girl face" ,
+        image:  "https://images.unsplash.com/photo-1630255733678-cf82ac59eef8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG1vZGVsJTIwZ2lybHxlbnwwfHwwfHx8MA%3D%3D"
+    },
+    {
+        name:   "girl face"  ,
+        image:  "https://images.unsplash.com/photo-1616683693504-3ea7e9ad6fec?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fG1vZGVsJTIwZ2lybHxlbnwwfHwwfHx8MA%3D%3D"
+    }, {
+        name:   "levis add"  ,
+        image:  "https://images.unsplash.com/photo-1516726817505-f5ed825624d8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fG1vZGVsJTIwZ2lybHxlbnwwfHwwfHx8MA%3D%3D"
+    },
+    {
+        name:  "garden girl"   ,
+        image:  "https://images.unsplash.com/photo-1503104834685-7205e8607eb9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fG1vZGVsJTIwZ2lybHxlbnwwfHwwfHx8MA%3D%3D"
+    }, {
+        name: "telephone girl"    ,
+        image:  "https://images.unsplash.com/photo-1618149103489-0e1210d6128a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzh8fG1vZGVsJTIwZ2lybHxlbnwwfHwwfHx8MA%3D%3D"
+    }, 
+    {
+        name:   "indian tradition"  ,
+        image:  "https://images.unsplash.com/photo-1617633150881-472457a1e1f7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTR8fG1vZGVsJTIwZ2lybHxlbnwwfHwwfHx8MA%3D%3D"
+    }, 
+    {
+        name:   "russian girl"  ,
+        image:  "https://images.unsplash.com/photo-1630255732364-a69ade0f0543?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTZ8fG1vZGVsJTIwZ2lybHxlbnwwfHwwfHx8MA%3D%3D"
+    }
+    , 
+    {
+        name:    "super model" ,
+        image: "https://images.unsplash.com/flagged/photo-1628336358297-50dfd92155c8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjR8fG1vZGVsJTIwZ2lybHxlbnwwfHwwfHx8MA%3D%3D" 
+    }
+    , 
+    {
+        name:    "feel the beat" ,
+        image:  "https://images.unsplash.com/photo-1616529735204-6799853ed36f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njh8fG1vZGVsJTIwZ2lybHxlbnwwfHwwfHx8MA%3D%3D"
+    }
+    , 
+    {
+        name:    "snow queen" ,
+        image:  "https://images.unsplash.com/photo-1616091093714-c64882e9ab55?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njd8fG1vZGVsJTIwZ2lybHxlbnwwfHwwfHx8MA%3D%3D"
+    }
+];
+
+function showTheCard() {
+    var clutter = "";
+    arr.forEach(function (obj) {
+        clutter += `<div class="box">
+        <img class="cursor-pointer" src="${obj.image}" alt="">
+        <div class="caption">  </div>
+    </div>`;
+    });
+    document.querySelector(".container").innerHTML = clutter;
+}
+
+
+function handleSearchFunctionality() {
+    var input = document.querySelector("#searchinput");
+    input.addEventListener("focus", function () {
+        document.querySelector(".overlay").style.display = "block";
+    });
+    input.addEventListener("blur", function () {
+        document.querySelector(".overlay").style.display = "none";
+    });
+    input.addEventListener("input",function(){
+        const filterArr = arr.filter(obj => obj.name.toLowerCase().startsWith(input.value));
+        var clutter=``; 
+    
+        filterArr.forEach(function(obj){
+            clutter += `<div class="res felx px-8 py-3">
+                <i class="ri-search-line font-semibold mr-5"></i>
+                <h3 class="font-semibold ">${obj.name}</h3>
+            </div>`;
+        })
+        var searchList = document.querySelector(".searchdata");
+        searchList.style.display= "block";
+        searchList.innerHTML = clutter;
+
+        input.addEventListener("blur", function () {
+            searchList.style.display = "none";
+        });
+    })
+
+}
+
+
+
+handleSearchFunctionality();
+showTheCard();
